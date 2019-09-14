29ada001671c9fcc37f31abe78e9b6bf5a9fe057
saheedandrew <andrewsaheed@gmail.com>
2019-09-14 10:27:27 -0400

    started on the frontend

--- text-visualizer/src/app/app.module.ts
+++ text-visualizer/src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { TextBoxComponent } from './text-box/text-box.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule, MatButtonModule} from '@angular/material';
+import { ImagesComponent } from './images/images.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TextBoxComponent,
+    ImagesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule, 
+    MatFormFieldModule, 
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
+    
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
